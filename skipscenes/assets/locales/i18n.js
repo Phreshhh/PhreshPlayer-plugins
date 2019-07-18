8371a2d9c77daf2196a8548a5af539a7457ae255
Phreshhh <thatismyspecial@gmail.com>
2019-07-18 22:19:43 +0200

    i18n refactoring

--- skipscenes/assets/locales/i18n.js
+++ skipscenes/assets/locales/i18n.js
@@ -1,19 +1,5 @@
-const electron = require('electron');
-const {remote} = require('electron');
-const path = require("path");
-const fs = require('fs');
-const Store = require('electron-store');
-const store = new Store();
-
 let loadedLanguage;
-let storedLanguage;
-
-if (store.has('settings.language')) {
-  storedLanguage = store.get('settings.language');
-} else {
-  store.set('settings.language', 'en');
-  storedLanguage = 'en';
-}
+let storedLanguage = store.get('settings.language');;
 
 let app = electron.app ? electron.app : electron.remote.app;
 
@@ -23,15 +9,15 @@ function i18n() {
 
   if (storedLanguage !== undefined && storedLanguage !== "") {
 
-    loadedLanguage = JSON.parse(fs.readFileSync(path.join(__dirname, storedLanguage + '.json'), 'utf8'));
+    loadedLanguage = JSON.parse(fse.readFileSync(path.join(__dirname, storedLanguage + '.json'), 'utf8'));
 
   } else {
 
-    if(fs.existsSync(path.join(__dirname, app.getLocale() + '.json'))) {
-      loadedLanguage = JSON.parse(fs.readFileSync(path.join(__dirname, app.getLocale() + '.json'), 'utf8'));
+    if(fse.existsSync(path.join(__dirname, app.getLocale() + '.json'))) {
+      loadedLanguage = JSON.parse(fse.readFileSync(path.join(__dirname, app.getLocale() + '.json'), 'utf8'));
     }
     else {
-      loadedLanguage = JSON.parse(fs.readFileSync(path.join(__dirname, 'en.json'), 'utf8'));
+      loadedLanguage = JSON.parse(fse.readFileSync(path.join(__dirname, 'en.json'), 'utf8'));
     }
 
   }
